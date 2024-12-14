795ab631c5214df2702da1014685c840b99287b3
Patrick Unick <dev_storm@winux.com>
2024-12-14 14:34:29 -0800

    [uni] update the server to be much more efficient. currently it is limited to one port.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,39 @@
-use tokio::net::UdpSocket;
-use std::collections::HashMap;
 use std::net::SocketAddr;
-use tokio::sync::Mutex;
+use tokio::net::UdpSocket;
 use std::sync::Arc;
-use std::io;
-
-#[tokio::main]
+use tokio::sync::Semaphore;
+use bytes::BytesMut;
+use dashmap::DashMap;
+use tokio::io;
+// 
+#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() -> io::Result<()> {
-    // Bind the proxy to port 5000
     let proxy_socket = UdpSocket::bind("127.0.0.1:5000").await?;
     println!("UDP proxy listening on 127.0.0.1:5000");
 
-    // Remote server address
     let server_addr: SocketAddr = "127.0.0.1:6000".parse().unwrap();
-
-    // Map to track client <-> server address associations
-    let client_map = Arc::new(Mutex::new(HashMap::<SocketAddr, SocketAddr>::new()));
+    let client_map = Arc::new(DashMap::<SocketAddr, SocketAddr>::new());
     let proxy_socket = Arc::new(proxy_socket);
 
-    let mut buf = vec![0u8; 1024];
+    // Shared buffer pool
+    let buf_pool = Arc::new(Semaphore::new(128));
 
     loop {
-        // Receive a packet from a client
-        let (len, client_addr) = proxy_socket.recv_from(&mut buf).await?;
-        println!("Received {} bytes from client {}", len, client_addr);
+        let proxy_socket = Arc::clone(&proxy_socket);
+        let client_map = Arc::clone(&client_map);
+        let server_addr = server_addr.clone();
+        let buf_pool = Arc::clone(&buf_pool);
 
-        // Forward the packet to the server
-        proxy_socket.send_to(&buf[..len], server_addr).await?;
-        println!("Forwarded {} bytes to server {}", len, server_addr);
+        tokio::spawn(async move {
+            let _permit = buf_pool.acquire().await.unwrap(); // Acquire buffer
+            let mut buf = BytesMut::with_capacity(64 * 1024);
 
-        // Receive a response from the server
-        let (len, server_response_addr) = proxy_socket.recv_from(&mut buf).await?;
-        println!(
-            "Received {} bytes from server {}",
-            len, server_response_addr
-        );
-
-        // Forward the server's response back to the client
-        proxy_socket.send_to(&buf[..len], client_addr).await?;
-        println!("Forwarded {} bytes back to client {}", len, client_addr);
+            if let Ok((len, client_addr)) = proxy_socket.recv_from(&mut buf).await {
+                client_map.insert(client_addr, server_addr);
+                if let Err(e) = proxy_socket.send_to(&buf[..len], server_addr).await {
+                    eprintln!("Error forwarding to server: {}", e);
+                }
+            }
+        });
     }
 }
