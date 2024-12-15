97951be1f69c40cea12eff1d3ada47c2a07bc7ea
Patrick Unick <dev_storm@winux.com>
2024-12-14 17:00:23 -0800

    [uni] created router

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,29 @@
-use std::net::SocketAddr;
-use tokio::net::UdpSocket;
-use std::sync::Arc;
-use tokio::sync::Semaphore;
-use bytes::BytesMut;
-use dashmap::DashMap;
+mod router;
+
 use tokio::io;
-// 
-#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
-async fn main() -> io::Result<()> {
-    let proxy_socket = UdpSocket::bind("127.0.0.1:5000").await?;
-    println!("UDP proxy listening on 127.0.0.1:5000");
+use crate::router::Router;
 
-    let server_addr: SocketAddr = "127.0.0.1:6000".parse().unwrap();
-    let client_map = Arc::new(DashMap::<SocketAddr, SocketAddr>::new());
-    let proxy_socket = Arc::new(proxy_socket);
-
-    // Shared buffer pool
-    let buf_pool = Arc::new(Semaphore::new(128));
-
-    loop {
-        let proxy_socket = Arc::clone(&proxy_socket);
-        let client_map = Arc::clone(&client_map);
-        let server_addr = server_addr.clone();
-        let buf_pool = Arc::clone(&buf_pool);
-
-        tokio::spawn(async move {
-            let _permit = buf_pool.acquire().await.unwrap(); // Acquire buffer
-            let mut buf = BytesMut::with_capacity(64 * 1024);
-
-            if let Ok((len, client_addr)) = proxy_socket.recv_from(&mut buf).await {
-                client_map.insert(client_addr, server_addr);
-                if let Err(e) = proxy_socket.send_to(&buf[..len], server_addr).await {
-                    eprintln!("Error forwarding to server: {}", e);
-                }
-            }
-        });
-    }
+fn bind_ports() {
+    
 }
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    // create our router manager
+    let router = Router::new().route(
+        "127.0.0.1:5000".parse().unwrap(),
+        "127.0.0.1:6000".parse().unwrap(),
+    );
+    
+    println!("{:?}", router.routes());
+    
+    Ok(())
+}
+
+// map
+// inputs -> outputs
+// inputs:x-y --1:1-> outputs:x-y
+// ^^^ i need to handle multiple rules sets for these
+
+// create a table of where things need to go
+
---
+++ src/router.rs
@@ -0,0 +1,54 @@
+use std::collections::HashSet;
+use std::sync::Arc;
+use dashmap::DashMap;
+use std::net::{IpAddr, SocketAddr};
+
+#[derive(Debug, Default)]
+pub struct Router {
+    routes: Arc<DashMap<SocketAddr, SocketAddr>>
+}
+
+
+impl Router {
+    pub fn new() -> Router {
+        Router {
+            routes: Arc::new(DashMap::new())
+        }
+    }
+    
+    pub fn add_route(&self, input: SocketAddr, output:SocketAddr) {
+        self.routes.insert(input, output);
+    }
+
+    pub fn route(self, input: SocketAddr, output:SocketAddr) -> Self {
+        self.add_route(input, output);
+        self
+    }
+    
+    
+    pub fn add_direct_routes<PortRange: Iterator<Item = u16>>(&self, input_ip: IpAddr, output_ip: IpAddr, port_range: PortRange) {
+        port_range.for_each(|port: u16| {
+            self.add_route(
+                SocketAddr::new(input_ip, port),
+                SocketAddr::new(output_ip, port)
+            );
+        });
+    }
+    
+    pub fn direct_routes<PortRange: Iterator<Item = u16>>(self, input_ip: IpAddr, output_ip: IpAddr, port_range: PortRange) -> Self {
+        self.add_direct_routes(input_ip, output_ip, port_range);
+        self
+    }
+    
+    /// method that calculates the ports that need to be bound based on routes
+    /// todo: rename this
+    pub fn required_ports(&self) -> HashSet<u16> {
+        self.routes.iter()
+            .map(|route| route.key().port())
+            .collect()
+    }
+    
+    pub fn routes(&self) -> Arc<DashMap<SocketAddr, SocketAddr>> {
+        Arc::clone(&self.routes)
+    }
+}
