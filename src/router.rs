9a6b6cf29a0f9c232c51a74a696fa94013ee75c3
Patrick Unick <dev_storm@winux.com>
2024-12-14 18:14:59 -0800

    [uni] more route handling

--- src/router.rs
+++ src/router.rs
@@ -25,7 +25,6 @@ impl Router {
         self
     }
     
-    
     pub fn add_direct_routes<PortRange: Iterator<Item = u16>>(&self, input_ip: IpAddr, output_ip: IpAddr, port_range: PortRange) {
         port_range.for_each(|port: u16| {
             self.add_route(
@@ -40,6 +39,45 @@ impl Router {
         self
     }
     
+    // returns None if it is impossible
+    pub fn add_offset_routes<PortRange: Iterator<Item = u16>>(
+        &self,
+        input_ip: IpAddr,
+        input_port_range: PortRange,
+        output_ip: IpAddr,
+        output_port_range: PortRange,
+    ) -> Option<()> {
+        // Use iterators to simultaneously iterate over both port ranges
+        let mut input_ports = input_port_range;
+        let mut output_ports = output_port_range;
+
+        loop {
+            match (input_ports.next(), output_ports.next()) {
+                (Some(input_port), Some(output_port)) => {
+                    self.add_route(
+                        SocketAddr::new(input_ip, input_port),
+                        SocketAddr::new(output_ip, output_port),
+                    );
+                }
+                (None, None) => break, // both ranges are fully iterated
+                _ => return None, // mismatched range lengths
+            }
+        }
+
+        Some(())
+    }
+
+    pub fn offset_routes<PortRange: Iterator<Item = u16>>(
+        self,
+        input_ip: IpAddr,
+        input_port_range: PortRange,
+        output_ip: IpAddr,
+        output_port_range: PortRange,
+    ) -> Option<Self> {
+        self.add_offset_routes(input_ip, input_port_range, output_ip, output_port_range);
+        Some(self)
+    }
+
     /// method that calculates the ports that need to be bound based on routes
     /// todo: rename this
     pub fn required_ports(&self) -> HashSet<u16> {
